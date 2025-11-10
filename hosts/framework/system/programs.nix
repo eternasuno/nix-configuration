306532513e6cfa7646abe6a160d67f5d2fdcfdf8
eternasuno <22316214+eternasuno@users.noreply.github.com>
2025-11-10 23:17:46 +0000

    fix: uncomment greetd service configuration for potential future use

--- hosts/framework/system/programs.nix
+++ hosts/framework/system/programs.nix
@@ -14,13 +14,19 @@ in
     inputs.dankMaterialShell.nixosModules.greeter
   ];
 
-  services.greetd = {
-    enable = true;
-  };
+  # services.greetd = {
+  #   enable = true;
+  #   settings = {
+  #     default_session = {
+  #       command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-user-session --asterisks --greeting Hello --cmd ${pkgs.niri}/bin/niri-session";
+  #       user = username;
+  #     };
+  #   };
+  # };
 
   programs.dankMaterialShell.greeter = {
     enable = true;
-    compositor.name = "niri";
+    compositor.name = "${pkgs.niri}/bin/niri-session";
     configHome = "/home/${username}";
     logs.save = true;
   };
