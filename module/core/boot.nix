1595b4a74e34575ecfd0d529c697db4c427924a2
eternasuno <22316214+eternasuno@users.noreply.github.com>
2026-01-31 11:41:11 +0900

    feat: update boot configuration with additional kernel parameters and modules

--- module/core/boot.nix
+++ module/core/boot.nix
@@ -1,10 +1,14 @@
-{ lib, pkgs, ... }:
+{ lib, pkgs, config, ... }:
 {
   boot = {
+    kernelPackages = pkgs.linuxPackages_zen;
+    kernelModules = ["v4l2loopback"];
+    extraModulePackages = [config.boot.kernelPackages.v4l2loopback];
+    kernel.sysctl = {"vm.max_map_count" = 2147483642;};
     loader = {
       efi.canTouchEfiVariables = true;
       systemd-boot.enable = lib.mkForce false;
-      timeout = 1;
+      timeout = null;
     };
     lanzaboote = {
       enable = true;
