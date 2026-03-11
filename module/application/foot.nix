75ad564d59e49732f71fd6cff54d4bc0e41fc406
eternasuno <22316214+eternasuno@users.noreply.github.com>
2026-03-11 10:08:51 +0900

    update

--- module/application/foot.nix
+++ module/application/foot.nix
@@ -7,6 +7,9 @@ in
     programs.foot = {
       enable = true;
       settings = {
+        main = {
+          font = "monospace:size=14";
+        };
         colors = {
           alpha = 0.9;
         };
---
+++ module/desktop/greeter.nix
@@ -0,0 +1,21 @@
+{
+  inputs,
+  vars,
+  ...
+}:
+let
+  inherit (vars) username;
+in
+{
+  imports = [
+    inputs.dankMaterialShell.nixosModules.greeter
+  ];
+
+  programs.dconf.enable = true;
+
+  programs.dank-material-shell.greeter = {
+    enable = true;
+    compositor.name = "niri";
+    configHome = "/home/${username}";
+  };
+}
