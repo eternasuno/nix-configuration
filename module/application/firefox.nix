574b5106d022c0b92e94a5fce58541b9281910fc
eternasuno <22316214+eternasuno@users.noreply.github.com>
2026-03-12 21:35:01 +0900

    feat: enhance Firefox configuration with forced extensions and updated search settings

--- module/application/firefox.nix
+++ module/application/firefox.nix
@@ -27,12 +27,26 @@ in
         DisableAppUpdate = true;
         DisableTelemetry = true;
         DontCheckDefaultBrowser = true;
+        ExtensionSettings = {
+          "extension@tabliss.io" = {
+            installation_mode = "force_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/extension@tabliss.io/latest.xpi";
+          };
+          "uBlock0@raymondhill.net" = {
+            installation_mode = "force_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/uBlock0@raymondhill.net/latest.xpi";
+          };
+        };
         RequestedLocales = [ "zh-CN" ];
       };
       profiles.default = {
         id = 0;
         name = "default";
         isDefault = true;
+        search = {
+          force = true;
+          default = "ddg";
+        };
         settings = {
           "intl.locale.requested" = "zh-CN";
           "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
